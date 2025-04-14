122cee1e3a52268eab4aeb0154f9bf97df622f41
edaagunes <edagunes00@hotmail.com>
2025-04-14 16:19:05 +0300

    Added Project02_ApiConsume

---
+++ NetCoreAI.Project02_APIConsumeUI/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NetCoreAI.Project02_APIConsumeUI.Dtos
+{
+	public class CreateCustomerDto
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public decimal Balance { get; set; }
+	}
+}
---
+++ NetCoreAI.Project02_APIConsumeUI/Dtos/ResultCustomerDto.cs
@@ -0,0 +1,10 @@
+﻿namespace NetCoreAI.Project02_APIConsumeUI.Dtos
+{
+	public class ResultCustomerDto
+	{
+		public int CustomerId { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public decimal Balance { get; set; }
+	}
+}
