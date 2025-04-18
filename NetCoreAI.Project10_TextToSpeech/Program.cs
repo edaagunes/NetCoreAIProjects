a8bd426ee2bafb1a2bbbf57e62890019b39f23d3
edaagunes <edagunes00@hotmail.com>
2025-04-18 14:53:45 +0300

    Added Project10_TextToSpeech

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+
+		speechSynthesizer.Volume = 50;
+		speechSynthesizer.Rate = -4;
+
+		Console.Write("Metni giriniz: ");
+		string input=Console.ReadLine();
+
+		if (!string.IsNullOrEmpty(input))
+		{
+			speechSynthesizer.Speak(input);
+		}
+
+		Console.ReadLine();
+	}
+}
